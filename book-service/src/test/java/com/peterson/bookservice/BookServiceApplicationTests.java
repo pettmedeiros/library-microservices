8534451b574f95de93ac5d6a4a4212a778177c2c
Peterson <pettm20@gmail.com>
2026-07-07 18:07:06 -0300

    chore: estrutura inicial do projeto

---
+++ book-service/src/test/java/com/peterson/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterson.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
